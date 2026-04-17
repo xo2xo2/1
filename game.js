e12ff2a62d3e9d9318a83176639a045a83b23844
dark <aliahmed545454p@example.com>
2026-04-17 20:59:58 +0300

    FKJG

--- game.js
+++ game.js
@@ -20,7 +20,7 @@ var vLS1c45217fb5c792042bfe = "1c45217fb5c792042bfe0587f3d5249c";
 
     function changeText() {
         const el = document.getElementById("mm-event-text");
-        if (el && el.textContent !== "Welcome to dark tool") {
+        if (el && el.textContent !== "Welcome to DARK XO tool") {
             el.textContent = "Welcome to dark tool";
         }
     }
