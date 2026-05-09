c3771a850d10193e85e404fc0d0614ea14aa6881
dark <aliahmed545454p@example.com>
2026-05-09 07:45:16 +0300

    DEDED

--- js/friends.js
+++ js/friends.js
@@ -2875,7 +2875,7 @@ window.addEventListener("load", function () {
           this.Sf.alpha = 0.55;
           this.Jf.zIndex = 2;
           this.Jf.alpha = 0.9;
-          this.Jf.beginFill(16711680);
+          this.Jf.beginFill(0xFFFFFF);
           this.Jf.drawCircle(0, 0, this.Kf * 0.12);
           this.Jf.endFill();
           this.Jf.lineStyle(1, "black");
