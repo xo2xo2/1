271c24ad2772bd63e68c1b3422f1a80bdfbc0363
platenxo <platenxo@gmail.com>
2026-05-14 00:36:54 +0300

    Update timmap.js

--- js/timmap.js
+++ js/timmap.js
@@ -5996,7 +5996,7 @@ function f(p) {
       var vThis64 = this;
       var v1557 = v1646.get()[0];
       v1641.toggle(vO46.co.bo);
-      v1637.text(vO47.U("index.game.result.continue"));
+      v1637.text(vO47.U(vO47.a("322a9a2153bfd6ae5b2699634caa73a42e272a212fa364bc")));
       v1637.html("Continue (Home)");
       v1637.after("<div id='final-replay'>Replay</div>")
       v1638.text(vO47.U(vO47.a("4078ecf721eda8702974efb53ef8fd6a3c55bcc03bcbe24e365cff")));
